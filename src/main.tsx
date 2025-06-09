ea3550dd969cd43947ac5148193077b6a79e964d
gercio <gerciobandep@gmail.com>
2025-06-09 21:13:41 +0200

    remove react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import "./index.css";
 import App from "./App.tsx";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router } from "react-router-dom";
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Router>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
